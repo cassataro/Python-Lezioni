769a2042b46bd3c0c65e6eda16a7c126e1dcb555
cassataro <cassataro@olomedia.it>
2022-09-15 14:21:56 +0200

    corretto file 3 della 4 lezione

--- Secondo Corso/eserrcizio-3.py
+++ Secondo Corso/eserrcizio-3.py
@@ -1,6 +1,6 @@
 ore_lavorative = float(input("Inserisci le tue ore lavorative: "))
 tariffa_oraria = int(10)
-ore_straordinario = int(15)
+ore_straordinario = tariffa_oraria * 1.5
 if ore_lavorative == 40:
     paga_totale = ore_lavorative * tariffa_oraria
     print("Ore lavorative:" , ore_lavorative)
