962bf105c8241a36499f2b6d4fd602b20f40871e
cassataro <cassataro@olomedia.it>
2022-09-15 14:25:07 +0200

    corretto file 3 della 4 lezione

--- Secondo Corso/eserrcizio-3.py
+++ Secondo Corso/eserrcizio-3.py
@@ -4,20 +4,20 @@ ore_straordinario = tariffa_oraria * 1.5
 if ore_lavorative == 40:
     paga_totale = ore_lavorative * tariffa_oraria
     print("Ore lavorative:" , ore_lavorative)
-    print("Tariffa Oraria: 10")
+    print("Tariffa Oraria:" , tariffa_oraria)
     print("Paga totale:" , paga_totale)
 print("")
 
 if ore_lavorative < 40:
     paga_totale = ore_lavorative * tariffa_oraria
     print("Ore lavorative:" , ore_lavorative)
-    print("Tariffa Oraria: 10")
+    print("Tariffa Oraria:" , tariffa_oraria)
     print("Paga totale:" , paga_totale)
 print("")
 
 if ore_lavorative > 40:
     paga_totale = ore_lavorative * ore_straordinario
     print("Ore lavorative:" , ore_lavorative)
-    print("Tariffa Oraria: 15")
+    print("Tariffa Oraria:" , ore_straordinario)
     print("Paga totale:" , paga_totale)
 print("")
