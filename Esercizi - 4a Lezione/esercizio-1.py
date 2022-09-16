11bb17fec4f67f297f428e90dbdc1a43dbb3e967
cassataro <cassataro@olomedia.it>
2022-09-16 09:59:54 +0200

    corretto files

--- Esercizi - 4a Lezione/esercizio-1.py
+++ Esercizi - 4a Lezione/esercizio-1.py
@@ -1,12 +1,12 @@
 nome_utente = input("Quale è il tuo nome? ")
 eta_utente = int(input("Quanti anni hai? "))
 if eta_utente < 18:
-    print("Ti diamo il benvenuto" , nome_utente , "! Sei un minorenne.")
+    print("Ti diamo il benvenuto", nome_utente, "! Sei un minorenne.")
 if eta_utente > 18:
-    print("Ti diamo il benvenuto" , nome_utente , "! Sei un maggiorenne.")
+    print("Ti diamo il benvenuto", nome_utente, "! Sei un maggiorenne.")
 
 password_sicura = int(input("Inserisci la tua password "))
 if password_sicura == 1234:
-    print ("Benvenuto" , nome_utente)
+    print("Benvenuto", nome_utente)
 else:
     print("Password non corretta")
--- Esercizi - 4a Lezione/eserrcizio-3.py
+++ Esercizi - 4a Lezione/eserrcizio-3.py
@@ -3,18 +3,18 @@ tariffa_oraria = int(10)
 ore_straordinario = tariffa_oraria * 1.5
 if ore_lavorative == 40:
     paga_totale = ore_lavorative * tariffa_oraria
-    print("Ore lavorative:" , ore_lavorative)
-    print("Tariffa Oraria:" , tariffa_oraria)
-    print("Paga totale:" , paga_totale)
+    print("Ore lavorative:", ore_lavorative)
+    print("Tariffa Oraria:", tariffa_oraria)
+    print("Paga totale:", paga_totale)
 
 if ore_lavorative < 40:
     paga_totale = ore_lavorative * tariffa_oraria
-    print("Ore lavorative:" , ore_lavorative)
-    print("Tariffa Oraria:" , tariffa_oraria)
-    print("Paga totale:" , paga_totale)
+    print("Ore lavorative:", ore_lavorative)
+    print("Tariffa Oraria:", tariffa_oraria)
+    print("Paga totale:", paga_totale)
 
 if ore_lavorative > 40:
     paga_totale = ore_lavorative * ore_straordinario
-    print("Ore lavorative:" , ore_lavorative)
-    print("Tariffa Oraria:" , ore_straordinario)
-    print("Paga totale:" , paga_totale)
+    print("Ore lavorative:", ore_lavorative)
+    print("Tariffa Oraria:", ore_straordinario)
+    print("Paga totale:", paga_totale)
