0acebba68715fb2f05cf57a5587d69699f10f0bb
cassataro <cassataro@olomedia.it>
2022-09-29 15:10:29 +0200

    Nuovi esercizi

--- Esercizi - 6a Lezione/esercizio-4.py
+++ Esercizi - 6a Lezione/esercizio-4.py
@@ -1,4 +1,5 @@
-print('Quale è il numero più piccolo?')
-numeri  = [7, 10, 15, 20, 3, 9, 18, 100, 125]
+numeri = [7, 10, 15, 20, 3, 9, 18, 100, 125]
+print('Quale è il numero più piccolo tra:', numeri)
+
 valore_minimo = min(numeri)
 print('Il numero più basso è:', valore_minimo)
