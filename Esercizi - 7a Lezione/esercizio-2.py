e0f4494d37181f7afd732709dd11aa773beb31dd
cassataro <cassataro@olomedia.it>
2022-09-30 14:42:41 +0200

    Nuovi esercizi

---
+++ Esercizi - 7a Lezione/esercizio-2.py
@@ -0,0 +1,29 @@
+import random
+print('Ciao, oggi giochiamo a morra cinese')
+
+computer = random.randint(0, 2)
+
+if computer == 0: computer ='s'
+
+elif computer == 1: computer ='f'
+
+else: computer = 'c'
+
+utente=input('Inserisci c per carta, s per sasso ed f per forbice: ')
+
+if utente!='s' and utente!='f' and utente!='c':
+
+    print('La lettera non corrisponde a nulla')
+
+elif utente==computer:
+
+    print('parità!')
+
+elif utente =='s' and computer =='f' or utente =='f' and computer =='c' or utente =='c' and computer =='s':
+
+    print('Hai vinto!')
+else:
+
+    print('Ha vinto il computer!')
+
+print(computer)
